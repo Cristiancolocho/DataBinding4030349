2df5679fb12be15f4db7df4660679f81b9d77b1f
CHATO LOKO <colochocristian72@gmail.com>
2024-07-17 18:33:54 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding4030349/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace DataBinding4030349
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
